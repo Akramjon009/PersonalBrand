2d44920ce980c0cef97a6728dad1414f4741248d
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-05-14 09:54:54 +0500

    Add project files.

---
+++ src/PersonalBrand.Infrastructure/InfrastructureDependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PersonalBrand.Application.Abstractions;
+using PersonalBrand.Infrastructure.Persistance;
+
+
+namespace PersonalBrand.Infrastructure
+{
+    public static class InfrastructureDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureDependencyInjection(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<IApplicationDbContext, PersonalBrandDbContext>(options =>
+            {
+                options
+                    .UseLazyLoadingProxies()
+                        .UseNpgsql(configuration.GetConnectionString("Def"));
+            });
+
+            return service;
+        }
+    }
+}
---
+++ src/PersonalBrand.UI/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PersonalBrand.Domain.Entities.Auth;
+using PersonalBrand.Domain.IdentityModels;
+
+namespace PersonalBrand.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<UserModel> _userManager;
+
+        public AuthController(UserManager<UserModel> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Register registerModel)
+        {
+            return Ok(registerModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login loginModel)
+        {
+            return Ok(loginModel);
+        }
+    }
+}
